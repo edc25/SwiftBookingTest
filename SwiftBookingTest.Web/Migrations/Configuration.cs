b15b23e7ad1320f19ef62aa9d32e866cfa70add2
Ed Clayton <edc25@cantab.net>
2016-01-27 10:51:28 +0700

    * Expand HomeController to cover the new methods. * Add DeliveryService to handle the business logic of booking deliveries * SwiftConnector as an injectable connector for Swift (in case we ever want to switch away from Swift - or in case the API substantially changes) * SwiftClient handles the actual communication in an injectable way.
    * DeliveryPoint seems like a good DDD name for the entities we're creating
    * Delivery stores results from the calls to Swift; makes things a bit neater and more idempotent
    
    * Split Index.cshtml out into a partial and a display template.
    
    * DeliveryDataAccessor is a thin wrapper for SwiftDemoContext. It doesn't quite follow the Repository pattern, but it would be mockable.
    
    * A couple of little tests for the Delivery Service

---
+++ SwiftBookingTest.Web/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace SwiftBookingTest.Web.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SwiftBookingTest.Web.Data.SwiftDemoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "SwiftBookingTest.Web.Data.SwiftDemoContext";
+        }
+
+        protected override void Seed(SwiftBookingTest.Web.Data.SwiftDemoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
